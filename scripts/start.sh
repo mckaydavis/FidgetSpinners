fdc01c0683820e84e5d6612e68e68aff447bf7f8
Jonathan Robello <jlarobello@live.com>
2017-09-03 22:14:14 -1000

    update scripts

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-(cd /home/ec2-user/server && npm start)
+cd /home/ec2-user/server
+npm start
--- scripts/unzip.sh
+++ scripts/unzip.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-(cp /tmp/server.zip /home/ec2-user/)
-(unzip -o /home/ec2-user/server.zip -d /home/ec2-user)
+cp /tmp/server.zip /home/ec2-user/
+unzip -o /home/ec2-user/server.zip -d /home/ec2-user
