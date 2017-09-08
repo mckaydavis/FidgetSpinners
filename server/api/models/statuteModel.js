b82c6a0eb959fab6c2db23bf9c81ca85e4c03e31
Terry Palomares <tpalomares18@gmail.com>
2017-09-08 00:52:16 -1000

    Created Statute Schema for mongoose.

---
+++ server/api/models/statuteModel.js
@@ -0,0 +1,29 @@
+'use strict';
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+var StatuteSchema = new Schema({
+  url: String,
+  year: String,
+  division: String,
+  division_text: String,
+  volume: String,
+  title: String,
+  title_text: String,
+  subtitle: String,
+  subtitle_text: String,
+  chapter: String,
+  chapter_text: String,
+  article: String,
+  article_text: String,
+  part: String, 
+  part_text: String,
+  subpart: String,
+  section: String,
+  section_text: String,
+  text: [String],
+  refs: [String]
+});
+
+module.exports = mongoose.model('Statute', StatuteSchema);
