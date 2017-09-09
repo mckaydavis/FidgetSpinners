a96de6422bc63f08399c110fe9389d27d10fc8dc
Christopher Oh <oh2@jabsomit.hawaii.edu>
2017-09-08 15:11:36 -1000

    upload a picture from gallery camera settings

--- client/src/pages/home/home.ts
+++ client/src/pages/home/home.ts
@@ -5,29 +5,39 @@ import { TitlePage } from '../title/title';
 import { ChapterPage } from '../chapter/chapter';
 import { SectionPage } from '../section/section';
 import { StatuePage } from '../statue/statue';
+import { Camera } from '@ionic-native/camera';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [[Camera]]
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private camera: Camera) {
   }
-  goToDivision(params){
+  goToDivision(params) {
     if (!params) params = {};
     this.navCtrl.push(DivisionPage);
-  }goToTitle(params){
+  } goToTitle(params) {
     if (!params) params = {};
     this.navCtrl.push(TitlePage);
-  }goToChapter(params){
+  } goToChapter(params) {
     if (!params) params = {};
     this.navCtrl.push(ChapterPage);
-  }goToSection(params){
+  } goToSection(params) {
     if (!params) params = {};
     this.navCtrl.push(SectionPage);
-  }goToStatue(params){
+  } goToStatue(params) {
     if (!params) params = {};
     this.navCtrl.push(StatuePage);
+  } askForPicture(): void {
+    let cameraOptions = {
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      quality: 100,
+      encodingType: this.camera.EncodingType.PNG,
+      correctOrientation: true
+    }
   }
 }
