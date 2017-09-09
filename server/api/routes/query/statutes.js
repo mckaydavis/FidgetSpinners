c3bd99604bcdaa1bbb914b40b4719cabbcd6eebe
Terry Palomares <tpalomares18@gmail.com>
2017-09-08 20:29:45 -1000

    Added query by division title and chapter.

--- server/api/routes/query/statutes.js
+++ server/api/routes/query/statutes.js
@@ -2,6 +2,9 @@ let express = require('express'),
 	services = require('../../services/services.js'),
     router  = express.Router();
 
-router.get('/', services.listAllStatutes)
+router.get('/', services.listAllStatutes);
+router.get('/division/:division', services.listByDivision);
+router.get('/title/:title', services.listByTitle);
+router.get('/chapter/:chapter', services.listByChapter);
 
 module.exports = router;
--- server/api/services/services.js
+++ server/api/services/services.js
@@ -10,3 +10,27 @@ exports.listAllStatutes = function(req, res) {
     res.json(statute);
   });
 };
+
+exports.listByDivision = function(req, res) {
+	Statute.find({division: req.params.division}, function(err, statute) {
+		if (err)
+			res.send(err);
+		res.json(statute);
+	});
+};
+
+exports.listByTitle = function(req, res) {
+	Statute.findOne({title: req.params.title}, function(err, statute) {
+		if (err)
+			res.send(err);
+		res.json(statute);
+	});
+};
+
+exports.listByChapter = function(req, res) {
+	Statute.findOne({chapter: req.params.chapter}, function(err, statute) {
+		if (err)
+			res.send(err);
+		res.json(statute);
+	});
+};
