bac8f7793930d363228dcfc6d1bade0318aa5bc4
Dan Paguirigan <danp@hawaii.edu>
2017-09-09 04:59:55 -1000

    fixed icons

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,11 +3,11 @@ import {Platform, Nav} from 'ionic-angular';
 import {StatusBar} from '@ionic-native/status-bar';
 import {SplashScreen} from '@ionic-native/splash-screen';
 
-import {DivisionPage} from '../pages/division/division';
-import {TitlePage} from '../pages/title/title';
-import {ChapterPage} from '../pages/chapter/chapter';
-import {SectionPage} from '../pages/section/section';
-import {StatuePage} from '../pages/statue/statue';
+// import {DivisionPage} from '../pages/division/division';
+// import {TitlePage} from '../pages/title/title';
+// import {ChapterPage} from '../pages/chapter/chapter';
+// import {SectionPage} from '../pages/section/section';
+// import {StatuePage} from '../pages/statue/statue';
 import {SettingsPage} from '../pages/settings/settings';
 import {HelpPage} from '../pages/help/help';
 import {BookmarksPage} from '../pages/bookmarks/bookmarks';
@@ -23,7 +23,7 @@ export class MyApp {
     @ViewChild(Nav) nav: Nav;
     rootPage: any = HomePage;
 
-    pages: Array<{ title: string, component: any }>;
+    pages: Array<{ title: string, component: any, icon: any }>;
 
     constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
         platform.ready().then(() => {
@@ -33,14 +33,15 @@ export class MyApp {
 
         // ngFor menu items
         this.pages = [
-            {title: 'Home', component: HomePage},
-            {title: 'Bookmarks', component: BookmarksPage},
-            {title: 'Help', component: HelpPage},
-            {title: 'Settings', component: SettingsPage},
+            {title: 'Home', component: HomePage, icon: 'ios-home'},
+            {title: 'Bookmarks', component: BookmarksPage, icon: 'ios-bookmark'},
+            {title: 'Help', component: HelpPage, icon: 'ios-help-circle-outline'},
+            {title: 'Settings', component: SettingsPage, icon: 'ios-settings'},
         ];
     }
 
     openPage(page) {
         this.nav.setRoot(page.component);
+        // active-highlight on menu items
     }
 }
