da27cce3360764b42bc89c4b1c9fe9b5e9a6c553
Chris Oh <oh2@hawaii.edu>
2017-09-12 17:07:30 -1000

    statute search service

--- client/src/pages/search/search.ts
+++ client/src/pages/search/search.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
-import { AppServer } from '../../services/appserver';
+import {Component} from '@angular/core';
+import {NavController, NavParams} from 'ionic-angular';
+import {StatuePage} from '../statue/statue';
+import {AppServer} from '../../services/appserver';
+import {Response} from '@angular/http';
 
 @Component({
   selector: 'page-search',
@@ -9,9 +11,66 @@ import { AppServer } from '../../services/appserver';
 export class SearchPage {
 
   private query: string;
+  private sections: any[] = null;
+  private loadingSections: boolean = false;
+  private section: any;
 
-  constructor(public navCtrl: NavController, private navParams: NavParams) {
+  constructor(public navCtrl: NavController, private navParams: NavParams, private server: AppServer) {
     this.query = navParams.get("query");
+    this.sections = [];
+    this.loadSections();
+  }
+
+  openSection(sec) {
+    this.server.getSection(sec._source.chapter, sec._source.section)
+      .map(response => response.json()).subscribe(result => this.section =result);
+
+    if(this.section != null) {
+      this.section = JSON.parse((JSON.stringify(this.section))
+        .substring(1, (JSON.stringify(this.section)).length-1));
+      this.navCtrl.push(StatuePage, {section: this.section});
+    }
+  }
+
+  sortSections() {
+    for (var a = 0; a < this.sections.length; a++) {
+      for (var b = a + 1; b < this.sections.length; b++) {
+        let seca = this.sections[a];
+        let secb = this.sections[b];
+        if (parseFloat(seca.section) > parseFloat(secb.section)) {
+          this.sections[a] = secb;
+          this.sections[b] = seca;
+        }
+      }
+    }
+  }
+
+  sectionsSuccess(res: Response) {
+    console.log("sectionsSuccess");
+    this.loadingSections = false;
+    try {
+      let that = this;
+      let jsonRes = res.json();
+      for (var a = 0; a < jsonRes.length; a++) {
+        this.sections.push(jsonRes[a]);
+      }
+      this.sortSections();
+    } catch (e) {
+      alert("Exception: " + e.message);
+    }
+  }
+
+  sectionsFailure(error: any) {
+    console.log("sectionsFailure");
+    this.loadingSections = false;
+    alert('Error: ' + JSON.stringify(error));
+  }
+
+  loadSections() {
+    this.loadingSections = true;
+    (this.server).getSearchQuery(this.query).subscribe(
+      res => this.sectionsSuccess(res), err => this.sectionsFailure(err)
+    );
   }
 
 }
--- client/src/services/appserver.ts
+++ client/src/services/appserver.ts
@@ -1,7 +1,7 @@
 /* * * ./app/comments/services/comment.service.ts * * */
 // Imports
-import { Injectable }     from '@angular/core';
-import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import {Injectable}     from '@angular/core';
+import {Http, Response, Headers, RequestOptions} from '@angular/http';
 //import {Observable} from 'rxjs/Rx';
 //Import RxJs required methods
 import 'rxjs/add/operator/map';
@@ -9,25 +9,36 @@ import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class AppServer {
-    
-    //private BASE_URL="http://192.168.8.107:9100/optdin/api/";
-    private BASE_URL="https://hrs.diblii.com/api/";
-    //https://hrs.diblii.com/api/chapter/1/section/
-    
-    // Resolve HTTP using the constructor
-    constructor (private http: Http) {}
 
-    getLocalJsonTree(){
-        console.log("getLocalJsonTree");
+  //private BASE_URL="http://192.168.8.107:9100/optdin/api/";
+  private BASE_URL = "https://hrs.diblii.com/api/";
+  //https://hrs.diblii.com/api/chapter/1/section/
 
-        let url="assets/hrstree.txt";
-        console.log(url);
-        return this.http.get(url);
-    }
+  // Resolve HTTP using the constructor
+  constructor(private http: Http) {
+  }
 
-    getSectionsForChapter(chapNum){
-        let url=this.BASE_URL+"chapter/"+chapNum+"/section/";
-        console.log(url);
-        return this.http.get(url);
-    }
+  getLocalJsonTree() {
+    console.log("getLocalJsonTree");
+
+    let url = "assets/hrstree.txt";
+    console.log(url);
+    return this.http.get(url);
+  }
+
+  getSectionsForChapter(chapNum) {
+    let url = this.BASE_URL + "chapter/" + chapNum + "/section/";
+    console.log(url);
+    return this.http.get(url);
+  }
+
+  getSection(chapNum, secNum) {
+    let url = this.BASE_URL + "chapter/" + chapNum + "/section/" + secNum;
+    return this.http.get(url);
+  }
+
+  getSearchQuery(query) {
+    let url = this.BASE_URL + "statutes/search?input=" + query + "&size=10";
+    return this.http.get(url);
+  }
 }
