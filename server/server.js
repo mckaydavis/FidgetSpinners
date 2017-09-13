870c8191901c17198daa92757f81e4c645154f35
Terry Palomares <tpalomares18@gmail.com>
2017-09-12 14:51:38 -1000

    Added corsheaders to API

--- server/server.js
+++ server/server.js
@@ -17,6 +17,12 @@ routes.forEach(function(route) {
     app.use('/api' + route.url, route.route);
 });
 
+app.use(function(req, res, next){
+res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', function(req, res) {
     res.send('Server Running!');
 });
