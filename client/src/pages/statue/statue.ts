cc1ec223b631347f0f1d034a0d02b973bc44302e
DanPaguirigan <danp@hawaii.edu>
2017-09-14 01:23:02 -1000

    statute navigation

--- client/src/pages/statue/statue.ts
+++ client/src/pages/statue/statue.ts
@@ -10,20 +10,46 @@ export class StatuePage {
 
   private section: any = null;
   private bookmarked: boolean = false;
+  private sectionsList: any[] = null;
+  private currentSecIndex: number = 0;
 
   constructor(public navCtrl: NavController,public navParams: NavParams,public server: AppServer) {
     this.section=this.navParams.get('section');
+    if (this.navParams.get('sectionsList')){
+      this.sectionsList=this.navParams.get('sectionsList');
+      this.currentSecIndex=this.navParams.get('sectionIndex');
+    }
     this.bookmarked=this.server.isInBookmark(this.section);
   }
 
   addToBookmark(){
     this.server.addToBookmark(this.section);
     this.bookmarked=true;
+    this.section.bookmarked=this.bookmarked;
   }
 
   removeFromBookmark(){
     this.server.removeFromBookmark(this.section);
     this.bookmarked=false;
+    this.section.bookmarked=this.bookmarked;
+  }
+
+  moveNext(){
+    if (this.sectionsList!=null){
+      if (this.currentSecIndex<(this.sectionsList.length-1)){
+        this.currentSecIndex++;
+        this.section=this.sectionsList[this.currentSecIndex];
+      }
+    }
+  }
+
+  movePrev(){
+    if (this.sectionsList!=null){
+      if (this.currentSecIndex>0){
+        this.currentSecIndex--;
+        this.section=this.sectionsList[this.currentSecIndex];
+      }
+    }
   }
 
 }
