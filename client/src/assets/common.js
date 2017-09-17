19f592d7882041bc3a134e795d27e3e936e4b9ec
Jonathan Robello <jlarobello@live.com>
2017-09-16 15:31:55 -1000

    remove console.log

--- client/src/assets/common.js
+++ client/src/assets/common.js
@@ -1,11 +1,9 @@
 var statueRef=null;
 
 function callShoutMe(ref,val){
-    console.log("callShoutMe: "+val);
     ref.shoutMe(val);
 }
 
 function callFromLink(val){
-    console.log("callFromLink: "+val);
     statueRef.shoutMe(val);
 }
