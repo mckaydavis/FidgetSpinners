8bb76085098e4266a2eb4cb2d4e4f3400608940d
Terry Palomares <tpalomares18@gmail.com>
2017-09-17 18:39:27 -1000

    trying to fix locatin

--- client/src/pages/location/location.ts
+++ client/src/pages/location/location.ts
@@ -20,7 +20,7 @@ export class LocationPage {
 
   private allSections: any[] = null;
   private sections: any[] = [];
-  private loadingSections: boolean = false;
+  private loadingSections: boolean = true;
   private jsonResLength: number;
   private lat: number;
   private lon: number;
@@ -71,37 +71,60 @@ export class LocationPage {
   }
 
   sectionsSuccess(res: Response) {
+    console.log('sectionssuccess');
     this.distances = [];
     this.allSections = [];
     this.sections = [];
-    this.loadingSections = false;
+
+
+    this.geolocation.getCurrentPosition().then((resp) => {
+
+      console.log("getting position");
     try {
       let jsonRes = res.json();
       this.jsonResLength = jsonRes.length;
+
       for (var a = 0; a < this.jsonResLength; a++) {
         let js = jsonRes[a];
         js.bookmarked = this.server.isInBookmark(js);
         this.allSections.push(js);
+
         if (a < 15) {
           this.sections[a] = this.allSections[a];
 
           try {
-            this.getLocation();
-            this.sections[a].distance = this.locationServer.getDistanceBetweenInKm(this.lon, this.lat,
-            this.sections[a].x, this.sections[a].y);
+              this.lat = resp.coords.latitude
+              this.lon = resp.coords.longitude
+
+              this.sections[a].distance = this.locationServer.getDistanceBetweenInKm(this.lon, this.lat,
+              this.sections[a].x, this.sections[a].y);
+
+
+              this.distance = this.sections[a].distance.toString();
+              this.distance = this.distance.substring(0,4);
+              this.sections[a].distance = parseFloat(this.distance);
+
+              console.log(this.distance)
+
           }catch(e) {
             this.sections[a].distance = this.locationServer.getDistanceBetweenInKm(21.2952, -157.8136,
             this.sections[a].x, this.sections[a].y);
+            this.loadingSections = false;
+
           }
 
-          this.distance = this.sections[a].distance.toString();
-          this.distance = this.distance.substring(0,4);
-          this.sections[a].distance = parseFloat(this.distance);
         }
       }
+
+      this.loadingSections = false;
     } catch (e) {
+      this.loadingSections = false;
       alert("Exception: " + e.message);
-    }
+    } this.loadingSections = false;
+    }).catch((error) => {
+      console.log('Error getting location', error);
+    });
+
   }
 
   sectionsFailure(error: any) {
